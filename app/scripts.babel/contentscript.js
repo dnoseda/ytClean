ce3552b8fe1d40560b385a69f5d0622662f0d076
dnoseda <damian.noseda@mercadolibre.com>
2017-04-01 12:06:27 -0300

    workng

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,26 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Content script');
+
+
+
+let cleanYt = function() {
+
+  let elementList = document.getElementsByTagName('ytd-grid-video-renderer');
+  for (var i = elementList.length - 1; i >= 0; i--) {
+    let element = elementList[i]
+  
+    let elementTitle = element.querySelector('span#video-title')
+    if(elementTitle){
+      let title = elementTitle.innerHTML.split('\n').join().toLowerCase();
+      if(title.match(/dark souls/) || title.match(/ds3/)){
+         let parent = element.parentElement;
+         parent.removeChild(element);
+         console.log('borrado elemento "'+title.trim()+'"');
+      }
+    }
+  }
+  setTimeout(cleanYt, 100);
+}
+
+setTimeout(cleanYt, 100);
