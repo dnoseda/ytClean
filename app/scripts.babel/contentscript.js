2d65168b08accb5c6013f4469af70bc6c40459ca
dnoseda <damian.noseda@mercadolibre.com>
2017-04-03 09:31:15 -0300

    taking the recommended as well

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -5,25 +5,29 @@ console.log('\'Allo \'Allo! Content script');
 
 
 let cleanYt = function() {
-
-  let elementList = document.getElementsByTagName('ytd-grid-video-renderer');
-  for (var i = elementList.length - 1; i >= 0; i--) {
-    let element = elementList[i]
+  let ytdRendererTypes = ['ytd-grid-video-renderer','ytd-compact-video-renderer']
+  for (var j = ytdRendererTypes.length - 1; j >= 0; j--) {
+    let ytdRendererType = ytdRendererTypes[j];
   
-    let elementTitle = element.querySelector('span#video-title')
-    if(elementTitle){
-      let title = elementTitle.innerHTML.split('\n').join().toLowerCase();
-      // TODO: Que quede configurado qué puede borrar
-      // TODO: que borre en una busqueda
-      // TODO: Que borre en sugeridos
-      if(title.match(/dark souls/) || title.match(/ds3/)){
-         let parent = element.parentElement;
-         parent.removeChild(element);
-         console.log('borrado elemento "'+title.trim()+'"');
+    let elementList = document.getElementsByTagName(ytdRendererType);
+    for (var i = elementList.length - 1; i >= 0; i--) {
+      let element = elementList[i]
+    
+      let elementTitle = element.querySelector('span#video-title')
+      if(elementTitle){
+        let title = elementTitle.innerHTML.split('\n').join().toLowerCase();
+        // TODO: Que quede configurado qué puede borrar
+        // TODO: que borre en una busqueda
+        // TODO Que borre en sugeridos
+        if(title.match(/dark souls/) || title.match(/ds3/)){
+           let parent = element.parentElement;
+           parent.removeChild(element);
+           console.log('borrado elemento "'+title.trim()+'"');
+        }
       }
     }
   }
   setTimeout(cleanYt, 100);
 }
 
-setTimeout(cleanYt, 100);
+cleanYt();
